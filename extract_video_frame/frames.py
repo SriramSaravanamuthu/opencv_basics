b79d1328bb19f2d9cb368c88c1f4c95765724235
SriramSaravanamuthu <sriram13101994@gmail.com>
2022-03-24 01:28:27 +0530

    Argparse added

--- extract_video_frame/frames.py
+++ extract_video_frame/frames.py
@@ -1,8 +1,9 @@
-
+import cv2
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i1", "--Input1", help = "Enter first image location")
+parser.add_argument("-i", "--Input1", help = "Enter first image location")
+parser.add_argument("-o", "--Output1", help = "Enter frame location")
 args = parser.parse_args()
 
 path = args.Input1
@@ -12,7 +13,7 @@ i = 1
 while cap.isOpened():
     ret,frame = cap.read()
     if ret:
-        cv2.imwrite('/home/sriram/Delete/'+'frame_'+str(i)+'.jpg',frame)
+        cv2.imwrite(args.Output1+'frame_'+str(i)+'.jpg',frame)
         i+=1
 #         cv2.imshow('Counter',frame)
 #         if cv2.waitKey() == ord('q'):
